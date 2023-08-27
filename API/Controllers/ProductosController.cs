5d064842d8312d092473c3f4ab6a7035e88589d7
Artemio <Artemio@DESKTOP-92BTGL5>
2023-08-27 15:11:15 -0300

    Added POST, PUT, DELETE Products

--- API/Controllers/ProductosController.cs
+++ API/Controllers/ProductosController.cs
@@ -31,4 +31,55 @@ public class ProductosController : BaseApiController
         var productos = await _unitOfWork.Productos.GetByIdAsync(id);
         return Ok(productos);
     }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Producto>> Post(Producto producto)
+    {
+        if (producto == null)
+        {
+            return BadRequest();
+        }
+
+        _unitOfWork.Productos.Add(producto);
+        _unitOfWork.Save();
+
+        return CreatedAtAction(nameof(Post), new { id = producto.Id }, producto);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Producto>> Put(int id, [FromBody] Producto producto)
+    {
+        if (producto == null)
+        {
+            return NotFound();
+        }
+
+        _unitOfWork.Productos.Update(producto);
+        _unitOfWork.Save();
+
+        return Ok(producto);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete (int id)
+    {
+        var producto = await _unitOfWork.Productos.GetByIdAsync(id);
+
+        if (producto == null)
+        {
+            return NotFound();
+        }
+
+        _unitOfWork.Productos.Remove(producto);
+        _unitOfWork.Save();
+
+        return NoContent();
+    }
 }
